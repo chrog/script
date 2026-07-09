79a5b86f7aec7f29c6b820fd8be8e9f51f19aa9d
chrog <rogowsky@chrog.net>
2026-07-09 07:24:38 +0200

    Update borg_backup.sh

--- borg/borg_backup.sh
+++ borg/borg_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export BORG_PASSPHRASE="user"
-borg create -v --stats -C lz4 /home/user/shares/backup/borg_repo::backup_$(date +%Y%m%d) /home/user
+borg create -v --stats --progress -C lz4 /home/user/shares/backup/borg_repo::backup_$(date +%Y%m%d) /home/user
